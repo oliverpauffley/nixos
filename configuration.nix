257c221f3b3fa4809870d683d397f52673533541
Ollie <mrpauffley@gmail.com>
2022-02-01 16:48:57 +0000

    diable fully

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
   ];
 
 
-  programs.steam.enable = true;
+  #programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
