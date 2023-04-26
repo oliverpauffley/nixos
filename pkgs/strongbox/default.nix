94ca78addf48cc324ac7b654836afeafc7ea185d
Ollie <mrpauffley@gmail.com>
2023-04-26 10:23:48 +0000

    strongbox-continued

--- pkgs/strongbox/default.nix
+++ pkgs/strongbox/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "strongbox";
-  version = "0.2.6";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "uw-labs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-GTEc9IqlfCW962/hXlEQ1ibXXbmjEfNXFVsuquUQdi8=";
+    sha256 = "sha256-ovuW/la0sRrIVCyxmdmSjxlhQT3duJrlebaTIJ9IxCs=";
   };
-  modSha256 = "sha256-E59VhZQQrzEgbD+ZVHvO0Dq4ytYJIjG+V+629l4B+Y0=";
-  vendorSha256 = "sha256-E59VhZQQrzEgbD+ZVHvO0Dq4ytYJIjG+V+629l4B+Y0=";
+  modSha256 = "sha256-E59VhZQQrzEgbD+ZVHvO0Dq4ytYJIjG+V+629l4B+YB=";
+  vendorSha256 = "sha256-E59VhZQQrzEgbD+ZVHvO0Dq4ytYJIjG+V+629l4B+YC=";
 
   meta = with lib; {
     description = "Encryption for git users";
