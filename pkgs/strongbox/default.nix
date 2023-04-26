e0ca151960717e6a085043e7bf59c70d044093e9
Ollie <mrpauffley@gmail.com>
2023-04-26 10:19:41 +0000

    strongbox

--- pkgs/strongbox/default.nix
+++ pkgs/strongbox/default.nix
@@ -5,7 +5,7 @@ buildGoModule rec {
   version = "0.2.6";
 
   src = fetchFromGitHub {
-    owner = "utilitywarehouse";
+    owner = "uw-labs";
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-GTEc9IqlfCW962/hXlEQ1ibXXbmjEfNXFVsuquUQdi8=";
@@ -14,7 +14,7 @@ buildGoModule rec {
   vendorSha256 = "sha256-E59VhZQQrzEgbD+ZVHvO0Dq4ytYJIjG+V+629l4B+Y0=";
 
   meta = with lib; {
-    description = "connects to utilitywarehouse vpn using wireguard";
+    description = "Encryption for git users";
     homepage = https://github.com/uw-labs/strongbox;
     platforms = platforms.all;
   };
